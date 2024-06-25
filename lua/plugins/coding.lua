bb59608fba1e94f7bfc3a51ce799e9a68342c455
m00nkod3 <joaopbmartins1981@gmail.com>
2024-06-25 19:37:20 +0000

    added custom keybind to accepting codeium suggestions

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -2,6 +2,13 @@ return {
   {
     "Exafunction/codeium.vim",
     event = "BufEnter",
+    config = function()
+      local keymap = vim.keymap
+      local opts = { expr = true, silent = true }
+      keymap.set("i", "<C-m>", function()
+        return vim.fn["codeium#Accept"]()
+      end, opts)
+    end,
   },
   {
     "smjonas/inc-rename.nvim",
